7672a2431c5b4642647765b768a71340615539c4
nulitas <ndrakyta@gmail.com>
2023-02-06 10:03:49 +0700

    Add style for button

--- src/main.js
+++ src/main.js
@@ -54,3 +54,20 @@ function createList(text) {
 
   notesContainer.insertAdjacentHTML("afterbegin", newNote);
 }
+
+let animateButton = function (e) {
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove("animate");
+
+  e.target.classList.add("animate");
+  setTimeout(function () {
+    e.target.classList.remove("animate");
+  }, 700);
+};
+
+let bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener("click", animateButton, false);
+}
